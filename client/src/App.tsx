ecaad6b2742c8c949540d4cd4babdcaf0dedb9bc
i.kłusek <J6ZkqwVUn>
2025-01-02 21:07:10 +0100

    TFM-18-Added mock movies data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
 import './styles/global.vars.scss';
 import './styles/global.scss';
+import { tinderMovies } from './mocks/movies';
 
 function App() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+      <img src={tinderMovies[1].imageUrl} alt="placeholder" />
+    </>
+  );
 }
 
 export default App;
