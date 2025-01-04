242c797f19c306ac8dae32b246321547598d0714
i.kłusek <J6ZkqwVUn>
2025-01-04 20:01:09 +0100

    TFM-23-Added side menu component

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -2,16 +2,18 @@ import KeyboardControlsLegend from '../../components/KeyboardControlsLegend/Keyb
 import SwipeCards from '../../components/SwipeCards/SwipeCards';
 import c from './Home.module.scss';
 import { useMoviesContext } from '../../context/MoviesContext';
+import SideMenu from '../../components/SideMenu/SideMenu';
 
 const Home = () => {
   const { movies } = useMoviesContext();
 
   return (
     <div className={c.wrapper}>
+      <SideMenu isVisible={false} onCloseCart={() => {}} />
       <div className={c.content}>
         <SwipeCards moviesData={movies} />
+        <KeyboardControlsLegend />
       </div>
-      <KeyboardControlsLegend />
     </div>
   );
 };
