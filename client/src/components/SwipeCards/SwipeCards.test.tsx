8afc24117f0a978c8654e91bfc40bbf8063ec187
i.kłusek <J6ZkqwVUn>
2025-01-05 17:33:21 +0100

    TFM-45-FIX- User could add the same movie items when clicked together keyboard arrows and card buttons

--- client/src/components/SwipeCards/SwipeCards.test.tsx
+++ client/src/components/SwipeCards/SwipeCards.test.tsx
@@ -24,12 +24,12 @@ const moviesData: Movie[] = [
 ];
 
 jest.mock('../Card/Card', () => {
-  const MockCard = ({ id, moveLeft, moveRight }: CardRef) => {
+  const MockCard = ({ id, addMovieToLiked, addMovieToDisliked }: CardRef) => {
     const ref: RefObject<HTMLDivElement> = useRef(null);
     return (
       <div data-testid={`card-${id}`} ref={ref}>
-        <button onClick={moveLeft}>Move Left</button>
-        <button onClick={moveRight}>Move Right</button>
+        <button onClick={addMovieToLiked}>Move Left</button>
+        <button onClick={addMovieToDisliked}>Move Right</button>
       </div>
     );
   };
@@ -40,7 +40,7 @@ jest.mock('../Card/Card', () => {
 });
 
 describe('SwipeCards', () => {
-  it('should trigger moveLeft function when ArrowLeft key is pressed', () => {
+  it('should trigger addMovieToLiked function when ArrowLeft key is pressed', () => {
     const { container } = render(<SwipeCards moviesData={moviesData} />);
 
     // Find the last card component
@@ -50,13 +50,13 @@ describe('SwipeCards', () => {
     // Simulate pressing the left arrow key
     fireEvent.keyDown(window, { key: 'ArrowLeft' });
 
-    // Check if the moveLeft function has been called
+    // Check if the addMovieToLiked function has been called
     // Here we simulate a simple console log to confirm the action
-    // Ideally, you'd spy on the moveLeft function in a real scenario
+    // Ideally, you'd spy on the addMovieToLiked function in a real scenario
     expect(lastCard).toHaveTextContent('Move Left');
   });
 
-  it('should trigger moveRight function when ArrowRight key is pressed', () => {
+  it('should trigger addMovieToDisliked function when ArrowRight key is pressed', () => {
     const { container } = render(<SwipeCards moviesData={moviesData} />);
 
     // Find the last card component
@@ -66,11 +66,11 @@ describe('SwipeCards', () => {
     // Simulate pressing the right arrow key
     fireEvent.keyDown(window, { key: 'ArrowRight' });
 
-    // Check if the moveRight function has been called
+    // Check if the addMovieToDisliked function has been called
     expect(lastCard).toHaveTextContent('Move Right');
   });
 
-  it('should not trigger moveLeft or moveRight if last card is not found', () => {
+  it('should not trigger addMovieToLiked or addMovieToDisliked if last card is not found', () => {
     const { container } = render(<SwipeCards moviesData={[]} />);
 
     // Simulate pressing the left arrow key in an empty list
